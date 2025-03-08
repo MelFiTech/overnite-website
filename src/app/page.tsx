3989e4ddcb02ac0fdb035e7f35bfed6eb6a9a498
60minuteman <6teeminuteman@gmail.com>
2025-03-08 09:32:28 +0100

    theme issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col font-[family-name:var(--font-spline-sans)] bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#1a1a2e] via-[#1a1a2e] to-[#2a1a3a]">
+    <div className="min-h-screen flex flex-col font-[family-name:var(--font-spline-sans)] bg-black">
       <Header />
       <main className="flex-1">
         <Hero />
